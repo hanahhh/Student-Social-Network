0d16eee471d35f0769fa8e33d609e9f67141d3b2
hanahhh <hoangnhung25122001@gmail.com>
2022-11-21 22:58:07 +0700

    client setup

--- routes/token.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { refreshTokenController } from "../controllers/tokenController.js";
-import { TryCatch } from "../middlewares/errorHandle.js";
-
-const router = express.Router();
-
-router.post("/refresh", TryCatch(refreshTokenController));
-export default router;
